a7411c72c52ac96b1a96ff4717ba8c52635e2327
clement <amlaganclement@gmail.com>
2026-03-25 14:53:37 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Dossier public pour CSS/JS
+app.use(express.static('public'));
+
+// Vue EJS
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('calculator');
+});
+
+
+app.listen(port, () => {
+  console.log(`Calculatrice EJS en ligne : http://localhost:${port}`);
+});
